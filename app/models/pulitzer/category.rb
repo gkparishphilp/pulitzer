146ab7719a1ef47c20714b75f9404f34aea0ae90
Michael Ferguson <meister@spacekace.com>
2018-09-18 13:49:18 -0700

    add title and to_s

--- app/models/pulitzer/category.rb
+++ app/models/pulitzer/category.rb
@@ -6,12 +6,20 @@ module Pulitzer
 		enum availability: { 'anyone' => 1, 'logged_in_users' => 2, 'just_me' => 3 }
 
 		belongs_to :parent, :class_name => "Pulitzer::Category", optional: true
-		
+
 		include FriendlyId
 		friendly_id :name, use: :slugged
 
 		def self.published( args = {} )
 			self.active.anyone
+		end
+
+		def title
+			self.name
+		end
+
+		def to_s
+			self.name
 		end
 
 	end
