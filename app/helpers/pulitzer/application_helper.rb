6365accab2362f3a134ffc4077cbd4691ae9eb12
Michael Ferguson <meister@spacekace.com>
2019-01-22 11:14:03 -0800

    bump version to add attachment helpers to pulitzer

--- app/helpers/pulitzer/application_helper.rb
+++ app/helpers/pulitzer/application_helper.rb
@@ -1,6 +1,111 @@
 module Pulitzer
 	module ApplicationHelper
-		
+
+		def attachment_resolutions(attachment, options={})
+			breakpoints_max = {
+				xs: 575,
+				sm: 767,
+				md: 991,
+				lg: 1199,
+				xl: nil,
+			}
+			breakpoints_min = {
+				xs: 0,
+				sm: 576,
+				md: 768,
+				lg: 992,
+				xl: 1200,
+			}
+
+			last_percent = nil
+			breakpoints_max.keys.each do |breakpoint|
+				if options[breakpoint] && options[breakpoint].end_with?('%x')
+					last_percent = options[breakpoint]
+				elsif options[breakpoint].present?
+					last_percent = nil
+				end
+
+				options[breakpoint] ||= last_percent
+			end
+
+			breakpoints = breakpoints_min.select{ |key,value| options[key].present? }
+
+			resolutions = []
+			breakpoints.each do |breakpoint,breakpoint_size|
+				size = options.delete(breakpoint)
+				next unless size.present?
+
+				if attachment.is_a? String
+					size_parts = size.split(' ', 2)
+					if size_parts.count > 1
+						src = size_parts.first
+					else
+						src = attachment
+					end
+
+					size = size_parts.last
+				else
+
+					size = "#{(breakpoints_max[breakpoint]).to_f / 100 * size[0..-3].to_f}x" if size.end_with?('%x') && breakpoints_max[breakpoint].present?
+					size = "auto" if size.end_with?('%x') && breakpoints_max[breakpoint].nil?
+					size = "#{attachment.blob.metadata['width']}x#{attachment.blob.metadata['height']}" if size.to_s == 'auto'
+					size = "#{size.split('x').first}x#{(size.split('x').first.to_f / attachment.blob.metadata['width'] * attachment.blob.metadata['height']).round(2)}" if size.last == 'x'
+					size = "#{(size.split('x').last.to_f / attachment.blob.metadata['height'] * attachment.blob.metadata['width']).round(2)}x#{size.split('x').last}" if size.first == 'x'
+
+					src = "#{attachment.variant(resize: size).processed.service_url}\##{size}"
+				end
+
+				resolutions << {
+					src: src,
+					width: size.split('x').first,
+					height: size.split('x').last,
+					breakpoint: breakpoint_size,
+				}
+			end
+
+			resolutions
+		end
+
+		# sample: = attachment_background_tag '#boo', ActiveStorage::Attachment.last, xs: '100x100', md: '500x500', lg: '1000x1000'
+		# sample: = attachment_background_tag '#boo', 'https://cdn1.neurohacker.com/uploads/Header-eaed8404-255c-4f31-a865-ee81e445f5b6.jpg', xs: 'https://wp.neurohacker.com/wp-content/uploads/2017/07/media_left.jpg 100x100', lg: 'https://cdn1.neurohacker.com/uploads/Headline_Bottles-7542e2fd-bb9b-47f2-bf9b-d96958b229be.png 200x200', style: 'width: auto !important;'
+		def attachment_background_tag(selector, attachment, options={})
+			resolutions = attachment_resolutions(attachment, options)
+			smallest_resolution = resolutions.sort_by{ |resolution| resolution[:width].to_f * resolution[:height].to_f }.first
+
+			content = <<-EOS
+#{selector} {
+	background-image: url('#{smallest_resolution[:src]}')
+}
+EOS
+			resolutions.each do |resolution|
+				content = content + <<-EOS
+@media(min-width: #{resolution[:breakpoint]}px) {
+	#{selector} {
+		background-image: url('#{resolution[:src]}')
+	}
+}
+EOS
+			end
+
+			content_tag( :style, raw( content ) )
+
+		end
+
+		# sample: = attachment_picture_tag ActiveStorage::Attachment.last, xs: '100x100', md: '500x500', lg: '1000x1000', style: 'width: auto !important;'
+		# sample: = attachment_picture_tag 'https://cdn1.neurohacker.com/uploads/Header-eaed8404-255c-4f31-a865-ee81e445f5b6.jpg', xs: 'https://wp.neurohacker.com/wp-content/uploads/2017/07/media_left.jpg 100x100', lg: 'https://cdn1.neurohacker.com/uploads/Headline_Bottles-7542e2fd-bb9b-47f2-bf9b-d96958b229be.png 200x200', style: 'width: auto !important;'
+		def attachment_picture_tag(attachment, options={})
+			resolutions = attachment_resolutions(attachment, options)
+			smallest_resolution = resolutions.sort_by{ |resolution| resolution[:width].to_f * resolution[:height].to_f }.first
+
+			content = image_tag( smallest_resolution[:src], options )
+			resolutions.each do |resolution|
+				content = content_tag( :source, '', media: "(min-width: #{resolution[:breakpoint]}px)", srcset: resolution[:src] ) + content
+			end
+
+			content_tag( :picture, content )
+
+		end
+
 		def set_css_if( args={} )
 			class_name = args[:class] || 'active'
 
@@ -21,7 +126,7 @@ module Pulitzer
 					return class_name if controller_name == controller.to_s
 				end
 			elsif args[:controller].present?
-				return class_name if controller_name == args[:controller].to_s 
+				return class_name if controller_name == args[:controller].to_s
 			end
 
 			return args[:else_class]
