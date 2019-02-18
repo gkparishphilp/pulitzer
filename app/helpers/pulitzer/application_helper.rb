1ed80dbdc2dcdac3f6af161fa3bd44931d78b847
Michael Ferguson <meister@spacekace.com>
2019-02-18 12:32:19 -0800

    again

--- app/helpers/pulitzer/application_helper.rb
+++ app/helpers/pulitzer/application_helper.rb
@@ -145,12 +145,12 @@ EOS
 				options['data-srcset'] = srcset
 				options['data-sizes'] = sizes
 
-				image_tag( 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=', options.merge( 'data-src' => attachment.service_url, class: "#{options[:class]} #{lazy}" ) )
+				image_tag( 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=', options.merge( 'data-src' => default_image, class: "#{options[:class]} #{lazy}" ) )
 			else
 				options['srcset'] = srcset
 				options['sizes'] = sizes
 
-				image_tag( attachment.service_url, options )
+				image_tag( default_image, options )
 			end
 
 			#if lazy
--- lib/pulitzer/version.rb
+++ lib/pulitzer/version.rb
@@ -1,3 +1,3 @@
 module Pulitzer
-  VERSION = '1.15.4'
+  VERSION = '1.15.5'
 end
