dd47d060e2adade0201cb7b38805626190769b80
Gk Parish-Philp <gk@gkparishphilp.com>
2019-05-15 16:17:25 -0700

    - set_site in app_cont

--- app/controllers/pulitzer/concerns/application_controller_concern.rb
+++ app/controllers/pulitzer/concerns/application_controller_concern.rb
@@ -22,59 +22,63 @@ module Pulitzer
 
 
 			protected
-			def client_ip
-				request.remote_ip
-			end
-
-			def client_ip_country
-				nil
-			end
-
-			def set_flash( msg, code=:success, *objs )
-				if flash[code].blank?
-					flash[code] = "<p>#{msg}</p>"
-				else
-					flash[code] += "<p>#{msg}</p>"
+				def client_ip
+					request.remote_ip
 				end
-				objs.each do |obj|
-					obj.errors.each do |error|
-						flash[code] += "<p>#{error.to_s}: #{obj.errors[error].join(';')}</p>"
+
+				def client_ip_country
+					nil
+				end
+
+				def set_flash( msg, code=:success, *objs )
+					if flash[code].blank?
+						flash[code] = "<p>#{msg}</p>"
+					else
+						flash[code] += "<p>#{msg}</p>"
+					end
+					objs.each do |obj|
+						obj.errors.each do |error|
+							flash[code] += "<p>#{error.to_s}: #{obj.errors[error].join(';')}</p>"
+						end
 					end
 				end
-			end
 
-			def add_page_event_data( args={} )
+				def add_page_event_data( args={} )
 
-				@page_event_data ||= []
+					@page_event_data ||= []
 
-				@page_event_data << args
+					@page_event_data << args
 
-			end
+				end
 
-			def set_page_meta( args={} )
+				def set_page_meta( args={} )
 
-				type = args[:type] || 'Article'
+					type = args[:type] || 'Article'
 
-				default_page_meta_og = ( Pulitzer.default_page_meta[:og] || {})
+					default_page_meta_og = ( Pulitzer.default_page_meta[:og] || {})
 
-				@page_meta = Pulitzer.default_page_meta.deep_merge({
-					title: args[:page_title] || args[:title] || Pulitzer.default_page_meta[:title] || Pulitzer.app_name,
-					description: args[:description] || Pulitzer.default_page_meta[:description] || Pulitzer.app_description,
-					url: request.url,
-					og: {
-						title: args[:title] || default_page_meta_og[:title] || Pulitzer.app_name,
-						type: type,
-						site_name: default_page_meta_og[:site_name] || Pulitzer.app_name,
+					@page_meta = Pulitzer.default_page_meta.deep_merge({
+						title: args[:page_title] || args[:title] || Pulitzer.default_page_meta[:title] || Pulitzer.app_name,
+						description: args[:description] || Pulitzer.default_page_meta[:description] || Pulitzer.app_description,
 						url: request.url,
-						description: args[:description] || default_page_meta_og[:description] || Pulitzer.app_description,
-						image: args[:image] || default_page_meta_og[:image] || Pulitzer.app_logo
-					}
-				})
+						og: {
+							title: args[:title] || default_page_meta_og[:title] || Pulitzer.app_name,
+							type: type,
+							site_name: default_page_meta_og[:site_name] || Pulitzer.app_name,
+							url: request.url,
+							description: args[:description] || default_page_meta_og[:description] || Pulitzer.app_description,
+							image: args[:image] || default_page_meta_og[:image] || Pulitzer.app_logo
+						}
+					})
 
-				@page_meta = @page_meta.deep_merge( args )
-				@page_meta[:schema] = { "@context" => "http://schema.org/", "@type" => type }.deep_merge( @page_meta[:data] ) if @page_meta[:data].present?
+					@page_meta = @page_meta.deep_merge( args )
+					@page_meta[:schema] = { "@context" => "http://schema.org/", "@type" => type }.deep_merge( @page_meta[:data] ) if @page_meta[:data].present?
 
-			end
+				end
+
+				def set_site
+					current_site = Site.find_by_domain( request.domain )
+				end
 
 
 
